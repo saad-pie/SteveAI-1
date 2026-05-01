ddece38185426055483ff9c6191f946af5790533
Saadpie1 <saadabdulrehman2010@gmail.com>
2026-05-01 17:35:50 +0500

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/SteveAI-v4/', 
+    base: '/SteveAI-1', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
